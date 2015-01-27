3cbf251b3bd31dddfa1a61c533227c8151a3486d
Armen Mkrtchyan <tankist@gmail.com>
2015-01-27 12:41:37 +0100

    tests for adapter integration

--- Tests/DependencyInjection/KreaitFirebaseExtensionTest.php
+++ Tests/DependencyInjection/KreaitFirebaseExtensionTest.php
@@ -40,4 +40,23 @@ class KreaitFirebaseExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($container->hasDefinition('kreait_firebase.reference.bar'));
         $this->assertTrue($container->hasDefinition('kreait_firebase.reference.bazz'));
     }
+
+    public function testWithAdapter()
+    {
+        $container = new ContainerBuilder();
+        $loader = new KreaitFirebaseExtension();
+        $loader->load(array(array('connections' => array(
+            'foo' => array(
+                'host' => 'foohost',
+                'adapter' => 'foohttpadapter'
+            )
+        ))), $container);
+
+        $definition = $container->getDefinition('kreait_firebase.connection.foo');
+        $this->assertAttributeCount(2, 'arguments', $definition);
+        $reference = $definition->getArgument(1);
+        
+        $this->assertInstanceOf('Symfony\Component\DependencyInjection\Reference', $reference);
+        $this->assertSame('ivory.http_adapter.foohttpadapter', (string)$reference);
+    }
 }
